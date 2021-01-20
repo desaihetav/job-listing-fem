d1453c5b095d132c94f51ce833239bd5af85ac75
desaihd <hetav.desai20@gmail.com>
2021-01-20 16:39:10 +0530

    create job card component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import JobCard from "./components/jobCard";
+// import data from "./data/data.json";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("./data/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, []);
+
+  console.log(jobs);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      {jobs.length === 0 ? (
+        <p>Fetching Jobs...</p>
+      ) : (
+        jobs.map((job) => <JobCard job={job} key={job.id} />)
+      )}
+      <JobCard />
     </div>
   );
 }
