3c5dd19c8c33e71722e78b5ff286434912f1ff6e
desaihd <hetav.desai20@gmail.com>
2021-01-21 00:59:55 +0530

    complete desktop ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { useState, useEffect } from "react";
 import JobCard from "./components/jobCard";
-// import data from "./data/data.json";
+import data from "./data/data.json";
 
 function App() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    fetch("./data/data.json")
-      .then((res) => res.json())
-      .then((data) => {
-        setJobs(data);
-      });
+    // fetch("./data/data.json")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setJobs(data);
+    //   });
+    setJobs(data);
   }, []);
 
   console.log(jobs);
 
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      {jobs.length === 0 ? (
-        <p>Fetching Jobs...</p>
-      ) : (
-        jobs.map((job) => <JobCard job={job} key={job.id} />)
-      )}
+    <div className="App bg-teal-50 m-0">
+      <header className="bg-teal-400 mb-10">
+        <img src="./images/bg-header-desktop.svg" className="w-full" />
+      </header>
+      {jobs &&
+        jobs.map((job) => {
+          console.log("Job Card Called");
+          return <JobCard job={job} key={job.id} />;
+        })}
       <JobCard />
     </div>
   );
--- src/components/jobCard.js
+++ src/components/jobCard.js
@@ -1,5 +1,73 @@
 import React from "react";
 
-export default function JobCard() {
-  return <div>Job Card</div>;
-}
+const JobCard = ({ job }) => {
+  if (job) {
+    const {
+      id,
+      company,
+      logo,
+      isNew,
+      isFeatured,
+      position,
+      role,
+      level,
+      postedAt,
+      contract,
+      location,
+      languages,
+      tools,
+    } = job;
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    return (
+      <div
+        className={`flex bg-white shadow-lg m-4 p-6 rounded-lg ${
+          isFeatured && "border-teal-500 border-l-4 border-solid"
+        }`}
+      >
+        <div>
+          <img src={logo} />
+        </div>
+        <div className="ml-4 flex justify-around flex-col mr-auto">
+          <h2 className="font-bold text-teal-500">
+            {company}
+            {isNew && (
+              <span className="ml-2 bg-teal-500 text-white px-2 py-1 rounded-2xl uppercase">
+                New!
+              </span>
+            )}
+            {isFeatured && (
+              <span className="ml-2 bg-gray-700 text-white px-2 py-1 rounded-2xl uppercase">
+                Featured
+              </span>
+            )}
+          </h2>
+          <h1 className="font-bold text-lg">{position}</h1>
+          <p className="text-gray-500">
+            {postedAt} • {contract} • {location}
+          </p>
+        </div>
+        <div className="flex justify-center items-center">
+          {tags.map((tag) => (
+            <>
+              <span className="bg-teal-50 text-teal-500 font-bold m-2 px-2 py-1 rounded-md">
+                {tag}
+              </span>
+            </>
+          ))}
+        </div>
+      </div>
+    );
+  }
+  return null;
+};
+
+export default JobCard;
