c23c949e90628d7c8b28430906f5d89a859b8237
desaihd <hetav.desai20@gmail.com>
2021-01-22 02:38:24 +0530

    add mobile UI and filtering

--- src/App.js
+++ src/App.js
@@ -1,30 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import JobCard from "./components/jobCard";
-import data from "./data/data.json";
 
 function App() {
   const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => {
-    // fetch("./data/data.json")
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setJobs(data);
-    //   });
-    setJobs(data);
+    fetch("./data/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
   }, []);
 
-  console.log(jobs);
+  const filteredJobs = jobs.filter(({ role, level, tools, languages }) => {
+    if (filters.length === 0) {
+      return true;
+    }
+
+    const tags = [role, level];
+
+    if (tools) {
+      tags.push(...tools);
+    }
+    if (languages) {
+      tags.push(...languages);
+    }
+
+    return filters.every((filter) => tags.includes(filter));
+  });
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) return;
+    setFilters([...filters, tag]);
+  };
+
+  const handleFilterClick = (filterToRemove) => {
+    setFilters(filters.filter((filter) => filter !== filterToRemove));
+  };
 
   return (
-    <div className="App bg-teal-50 m-0">
+    <div className="App w-full min-h-screen pb-2 bg-teal-50 m-0 select-none">
       <header className="bg-teal-400 mb-10">
-        <img src="./images/bg-header-desktop.svg" className="w-full" />
+        <img
+          src="./images/bg-header-desktop.svg"
+          className="w-full lg:h-auto h-36 object-cover"
+          alt="desktop header"
+        />
       </header>
-      {jobs &&
-        jobs.map((job) => {
-          console.log("Job Card Called");
-          return <JobCard job={job} key={job.id} />;
+      {filters.length > 0 && (
+        <div className="flex flex-row bg-white items-center shadow-lg justify-between mx-4 my-8 py-6 lg:px-12 px-6 rounded-lg">
+          <div className="flex flex-wrap">
+            {filters.map((filter) => (
+              <div className="mr-4 my-2">
+                <span className="bg-teal-50 text-teal-500 font-bold my-2 ml-0 px-2 py-1 rounded-l-md cursor-default">
+                  {filter}
+                </span>
+                <span
+                  className="bg-teal-500 px-2 py-1 rounded-r-md font-bold text-white hover:bg-gray-800 cursor-pointer"
+                  onClick={() => handleFilterClick(filter)}
+                >
+                  X
+                </span>
+              </div>
+            ))}
+          </div>
+          <span
+            onClick={() => setFilters([])}
+            className="px-2 font-bold text-gray-500 hover:text-teal-500 hover:underline duration-200 cursor-pointer"
+          >
+            Clear
+          </span>
+        </div>
+      )}
+      {filteredJobs &&
+        filteredJobs.map((job) => {
+          return (
+            <JobCard job={job} key={job.id} handleTagClick={handleTagClick} />
+          );
         })}
       <JobCard />
     </div>
--- src/components/jobCard.js
+++ src/components/jobCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const JobCard = ({ job }) => {
+const JobCard = ({ job, handleTagClick }) => {
   if (job) {
     const {
       id,
@@ -29,36 +29,44 @@ const JobCard = ({ job }) => {
 
     return (
       <div
-        className={`flex bg-white shadow-lg m-4 p-6 rounded-lg ${
+        className={`lg:flex-row flex flex-col lg:items-center bg-white shadow-lg lg:m-8 mx-4 mt-14 mb-10 p-6 rounded-lg ${
           isFeatured && "border-teal-500 border-l-4 border-solid"
         }`}
       >
         <div>
-          <img src={logo} />
+          <img
+            className="w-16 h-16 -mt-14 lg:w-full lg:h-full lg:m-0"
+            src={logo}
+          />
         </div>
-        <div className="ml-4 flex justify-around flex-col mr-auto">
-          <h2 className="font-bold text-teal-500">
+        <div className="lg:ml-4 my-4 lg:my-0 pb-4 lg:pb-0 flex justify-around flex-col mr-auto border-b lg:border-none w-full lg:w-auto">
+          <h2 className="font-bold my-2 text-teal-500">
             {company}
             {isNew && (
-              <span className="ml-2 bg-teal-500 text-white px-2 py-1 rounded-2xl uppercase">
+              <span className="ml-2 text-xs bg-teal-500 text-white px-2 py-1 rounded-2xl uppercase">
                 New!
               </span>
             )}
             {isFeatured && (
-              <span className="ml-2 bg-gray-700 text-white px-2 py-1 rounded-2xl uppercase">
+              <span className="ml-2 text-xs bg-gray-700 text-white px-2 py-1 rounded-2xl uppercase">
                 Featured
               </span>
             )}
           </h2>
-          <h1 className="font-bold text-lg">{position}</h1>
+          <h1 className="font-bold text-lg hover:text-teal-500 transition-colors duration-300">
+            {position}
+          </h1>
           <p className="text-gray-500">
             {postedAt} • {contract} • {location}
           </p>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex flex-wrap lg:justify-center items-center">
           {tags.map((tag) => (
             <>
-              <span className="bg-teal-50 text-teal-500 font-bold m-2 px-2 py-1 rounded-md">
+              <span
+                onClick={() => handleTagClick(tag)}
+                className="bg-teal-50 text-teal-500 font-bold m-2 ml-0 px-2 py-1 rounded-md cursor-pointer hover:bg-teal-500 hover:text-white transition-colors duration-300"
+              >
                 {tag}
               </span>
             </>
